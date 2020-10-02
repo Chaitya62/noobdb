5d5cd268d0ff42805751393bc4bc21f3b87e2fdb
Chaitya Shah <chaitya@browserstack.com>
2020-10-02 23:53:17 +0530

    Add todo search helper

--- README.md
+++ README.md
@@ -13,6 +13,6 @@
 ```
 
 ## TODOs
-- Add script to search `//TODO:` from the CODE and generate README.md in pre commit hook
+[x] Add script to search `//TODO:` from the CODE ~~and generate README.md in pre commit hook~~
 
 
--- util.sh
+++ util.sh
@@ -10,18 +10,24 @@ run_test() {
   go test ./... -v
 }
 
+show_todos() {
+  ag "//TODO:" --nofilename --ignore README.md --ignore util.sh --vimgrep | sed "s/\/\/TODO://g" | sed -e 's/^[[:space:]]*//' | awk '{ print $0}' 
+}
+
 unset ACTION
 
-while getopts 'ct' c
+while getopts 'ctp' c
 do
   case $c in
     c) ACTION=COVERAGE;;
-    t) ACTION=RUNTEST
+    t) ACTION=RUNTEST;;
+    p) ACTION=SHOWTODOS
   esac
 done
 
 # this only does one thing at a time 
 case $ACTION in
   COVERAGE) coverage;;
-  RUNTEST) run_test
+  RUNTEST) run_test;;
+  SHOWTODOS) show_todos
 esac
